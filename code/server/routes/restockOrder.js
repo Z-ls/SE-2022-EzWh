5c049b4564657157b1821b04b6af815be5def506
Riccardo Salvatelli <s297721@studenti.polito.it>
2022-05-10 19:40:45 +0200

    Restock order APIs completed

--- code/server/routes/restockOrder.js
+++ code/server/routes/restockOrder.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const restockOrderController = require('../controllers/restockOrderController');
 const router = express.Router();
-router.get('/restockOrders/:id', restockOrderController.getRestockOrder);
+
+const roc = new restockOrderController();
+
+router.get('/restockOrders/:id', roc.getRestockOrder);
+router.get('/restockOrders', roc.getAll);
+router.get('/restockOrdersIssued', roc.getAllIssued);
+router.get('/restockOrders/:id/returnItems', roc.returnItems);
+router.post('/restockOrder', roc.add);
+router.put('/restockOrder/:id', roc.updateState);
+router.put('/restockOrder/:id/skuItems', roc.addSKUItems);
+router.put('/restockOrder/:id/transportNote', roc.addTransportNote);
+router.delete('/restockOrder/:id', roc.delete);
+
 
 module.exports = router;
