b2bd73f27e5d5920967d2a22def4dbbc0cc43ec8
Edoardo Novara <s258553@studenti.polito.it>
2022-05-15 18:17:44 +0200

    Replace returnOrder.js

--- code/server/routes/returnOrder.js
+++ code/server/routes/returnOrder.js
@@ -1,14 +1,13 @@
 const express = require('express');
-
+const returnController = require('../controllers/returnOrderController');
 const router = express.Router();
 
-const returnController = require('../controllers/returnOrderController');
 
 router.get('/returnOrders', returnController.getReturnOrders);
-/* router.get('/returnOrders/:id', returnController.getReturnOrderByID);
+router.get('/returnOrders/:id', returnController.getReturnOrdersByID);
 
 router.post('/returnOrder', returnController.addReturnOrder);
 
 router.delete('/returnOrder/:id', returnController.deleteReturnOrder);
- */
+
 module.exports = router;
