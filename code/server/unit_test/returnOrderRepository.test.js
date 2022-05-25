07bc25a8ca01179857b9791cb9e2f8eda4744f17
Edoardo Novara <s258553@studenti.polito.it>
2022-05-25 23:30:06 +0200

    Replace returnOrderRepository.test.js

--- code/server/unit_test/returnOrderRepository.test.js
+++ code/server/unit_test/returnOrderRepository.test.js
@@ -5,14 +5,12 @@ const Item = require('../model/item');
 const Position = require('../model/position');
 const dayjs = require('dayjs');
 const DateHandler = require('../persistence/dateHandler');
-const retOrd = require('..model/returnOrder')
+const retOrd = require('../model/returnOrder')
 
 const dateHandler = new DateHandler();
 const DBHandler = require("../persistence/DBHandler");
 const skuRepository = require('../persistence/skuRepository');
-const testResult = require('../model/TestResult');
 const TestDescriptorRepository = require('../persistence/testDescriptorRepository');
-const TestResultRepository = require('../persistence/testResultRepository');
 const posRepository = require('../persistence/positionRepository');
 const restockOrderRepository = require('../persistence/restockOrderRepository');
 const userRepository = require('../persistence/userRepository');
@@ -22,7 +20,6 @@ const retRepository = require ('../persistence/returnOrderRepository')
 
 const dbHandler = new DBHandler();
 const testDescriptorRepo = new TestDescriptorRepository();
-const testResultRepo = new TestResultRepository();
 const restockRepo = new restockOrderRepository();
 const skuRepo = new skuRepository();
 const itemRepo = new itemRepository();
@@ -30,7 +27,7 @@ const positionRepo = new posRepository();
 const userRepo = new userRepository();
 const retRep = new retRepository();
 
-describe('add return Order', () => {
+describe('add return Order and retrieve it', () => {
   const rfid1 = "12345678901234567890123456789016";
   const rfid2 = "12345678901234567890123456789017";
   beforeEach(async () => {
@@ -39,7 +36,6 @@ describe('add return Order', () => {
     await skuRepo.addSKU(new SKU(1, "sku description", 2, 3, "note", "800234543412", 5, 10, [1]));
     await userRepo.add(new User(1, "Franco", "Panella", "franco.panella", "password1234", "supplier"));
     await itemRepo.addItem(new Item(1, "item description", 10, 1, 1));
-    await testDescriptorRepo.addTestDescriptor(new TestDescriptor('test descriptor', 'procedure description', 1));
 
     await restockRepo.add(new RestockOrder(undefined, dateHandler.DayjsToDateAndTime(dayjs()), "ISSUED",
       [
@@ -47,26 +43,26 @@ describe('add return Order', () => {
       ]
       , 1, {}, []));
 
+
     await restockRepo.updateState(1, "DELIVERED");
     await restockRepo.addSKUItems(1, [
       { SKUId: 1, rfid: rfid1 },
       { SKUId: 1, rfid: rfid2 }
     ]
     );
-    await restockRepo.updateState(1, "COMPLETEDRETURN");
   });
-  testAddRO(1, [new retOrd(1, dayjs(),   [
-    { SKUId: 1, description: "item description", price: 3, RFID : "12345678901234567890123456789016" }
-  ] ,1)]), "2021/11/29 09:33", [{SKUid: 1, description: "item description", price: 3, RFID: "12345678901234567890123456789016"}], 1;
-
-
+  const products = [{ SKUId: 1, description: "item description", price: 10, RFID : "12345678901234567890123456789016" }]
+  testAddRO(1, [new retOrd(1, "2021/11/29 09:33",   [
+    { SKUId: 1, description: "item description", price: 10, RFID : "12345678901234567890123456789016" }
+  ] ,1)], "2021/11/29 09:33", products, 1);
 });
 
-
 function testAddRO(id, expected, returnDate, products, restockOrderID) {
-  test('add return Order', async () => {
-  await retRep.addReturnOrder(returnDate, products, restockOrderID);
-  let  res = await retRep.get(id);
+  test('add return Order + add return Order transaction + get return Order', async () => {
+  await retRep.addReturnOrder(returnDate, restockOrderID);
+  await retRep.addReturnOrderTransaction(id, products);
+  let  res = await retRep.getReturnOrderbyID(id);
+  res[0].products= await retRep.getProductsbyID(id);
   expect(res).toEqual(expected);
   });
 }
